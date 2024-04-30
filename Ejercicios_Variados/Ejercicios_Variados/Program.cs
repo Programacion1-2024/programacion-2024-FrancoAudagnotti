4173b844cb57225f6d18ae1d88af25fe0df054d8
FrancoCarlosAudagnotti <166541585+FrancoCarlosAudagnotti@users.noreply.github.com>
2024-04-30 17:58:19 -0300

    Ejercicios Varios
    Ejercicios 1 y 2 echos

---
+++ Ejercicios_Variados/Ejercicios_Variados/Program.cs
@@ -0,0 +1,90 @@
+﻿////Ejercicio 1
+
+//float precio;
+//int cantidad;
+//int codigo;
+//int cantidadMax = 0;
+//int codigoMax = 0;
+
+//do
+//{
+//    Console.Write("Ingrese el código del producto: ");
+//    if (int.TryParse(Console.ReadLine(), out codigo) && codigo != 0)
+//    {
+//        Console.Write($"Ingrese el precio del producto {codigo}: ");
+//        if (float.TryParse(Console.ReadLine(),out precio))
+//        {
+//            Console.Write($"Ingrese la cantidad del producto {codigo}: ");
+//            if (int.TryParse(Console.ReadLine(), out cantidad))
+//            {
+//                if(cantidad > cantidadMax)
+//                {
+//                    cantidadMax = cantidad;
+//                    codigoMax = codigo;
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine("Error, ingrese un codigo válido.");
+//            }
+//        }
+//        else
+//        {
+//            Console.WriteLine("Error, ingrese un codigo válido.");
+//        }
+//    }
+//    else if (codigo != 0)
+//    {
+//        Console.WriteLine("Error, ingrese un codigo válido.");
+//    }
+//} while (codigo != 0);
+//Console.WriteLine($"El codigo que tiene más cantidad es el {codigoMax}");
+
+////Ejercicio 2
+
+//float precio;
+//int cantidadEnStock;
+//int cantidadDeVentas;
+//int codigo;
+//int cantidadNueva;
+//int codigoMax = 0;
+
+//do
+//{
+//    Console.Write("Ingrese el código del producto: ");
+//    if (int.TryParse(Console.ReadLine(), out codigo) && codigo != 0)
+//    {
+//        Console.Write($"Ingrese el precio del producto {codigo}: ");
+//        if (float.TryParse(Console.ReadLine(), out precio))
+//        {
+//            Console.Write($"Ingrese la cantidad en stock del producto {codigo}: ");
+//            if (int.TryParse(Console.ReadLine(), out cantidadEnStock))
+//            {
+//                Console.Write($"Ingrese la cantidad vendida: ");
+//                if (int.TryParse(Console.ReadLine(), out cantidadDeVentas))
+//                {
+//                    cantidadNueva = cantidadEnStock - cantidadDeVentas;
+//                    if (cantidadNueva <= 0)
+//                    {
+//                        Console.WriteLine($"No quedan más productos con el código {codigo} en stock.");
+//                    }
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine("Error, ingrese un codigo válido.");
+//            }
+//        }
+//        else
+//        {
+//            Console.WriteLine("Error, ingrese un codigo válido.");
+//        }
+//    }
+//    else if (codigo != 0)
+//    {
+//        Console.WriteLine("Error, ingrese un codigo válido.");
+//    }
+//} while (codigo != 0);
+//Console.WriteLine($"El codigo que tiene más cantidad es el {codigoMax}");
+
+
