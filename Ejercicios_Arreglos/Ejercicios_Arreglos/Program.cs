45a0f7a97e7d212227e731066cea4a6e7c6c016f
FrancoCarlosAudagnotti <166541585+FrancoCarlosAudagnotti@users.noreply.github.com>
2024-05-08 19:18:09 -0300

    Ejercicio arreglos
    a

---
+++ Ejercicios_Arreglos/Ejercicios_Arreglos/Program.cs
@@ -0,0 +1,36 @@
+﻿////Ejercicio 1
+//int numero;
+//int[] arregloNumeros = new int[5];
+
+//for (int i = 0; i < arregloNumeros.Length; i++)
+//{
+//    Console.Write("Ingrese un número: ");
+//    if (int.TryParse(Console.ReadLine(), out numero))
+//    {
+//        arregloNumeros[i] = numero;
+//    }
+//}
+//Console.WriteLine($"El arreglo es: ({arregloNumeros[0]}, {arregloNumeros[1]}, {arregloNumeros[2]}, {arregloNumeros[3]}, {arregloNumeros[4]})");
+
+//Ejercicio 2
+List<int> numerosEnteros = new List<int>();
+int numeroIngresado = 1;
+do
+{
+    Console.Write("Ingrese un número entero:");
+    if (int.TryParse(Console.ReadLine(), out numeroIngresado) && numeroIngresado != 0)
+    {
+        numerosEnteros.Add(numeroIngresado);
+    }
+
+} while (numeroIngresado != 0);
+
+var n = numerosEnteros.OrderByDescending(num => num);
+
+foreach(int numeroEntero in n)
+{
+    if (numeroEntero % 2 == 0)
+    {
+        Console.WriteLine($"{numeroEntero} ,");
+    }
+}
