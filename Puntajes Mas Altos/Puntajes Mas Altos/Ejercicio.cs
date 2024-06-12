09d1fdef2288b94f38bac66ad7cce73e4fa14bf1
FrancoCarlosAudagnotti <166541585+FrancoCarlosAudagnotti@users.noreply.github.com>
2024-06-12 19:40:49 -0300

    La deliciosa lasagna de Luciano
    Echo.

---
+++ Puntajes Mas Altos/Puntajes Mas Altos/Ejercicio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Puntajes_Mas_Altos
+{
+    /*
+       Consigna: 
+           Administre la lista de puntuaciones más altas de un jugador.
+
+           Tu tarea es crear un componente de puntuación alta del clásico juego Frogger, uno de los juegos más vendidos y adictivos de todos los tiempos, y un clásico de la era arcade.
+           Su tarea es escribir métodos que devuelvan la puntuación más alta de la lista, la última puntuación agregada y las tres puntuaciones más altas.
+   */
+
+    public class HighScores
+    {
+        public HighScores(List<int> list)
+        {
+            
+        }
+
+        public List<int> Scores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Latest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PersonalBest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> PersonalTopThree()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
