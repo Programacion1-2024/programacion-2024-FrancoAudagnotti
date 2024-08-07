5cc7f5af1d664c955a12d7baad179d4dbdfac160
FrancoCarlosAudagnotti <166541585+FrancoCarlosAudagnotti@users.noreply.github.com>
2024-08-07 19:26:04 -0300

    a
    a

--- Libreria/CEntidades/Entidades/Persona.cs
+++ Libreria/CEntidades/Entidades/Persona.cs
@@ -10,11 +10,11 @@ namespace CEntidades.Entidades
     public class Persona
     {
         public int IdPersona { get; set; }  
-        public long DNI { get; set; }
+        public long? DNI { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set;}
-        public string Nacionalidad { get; set; }
-        public string Telefono { get; set; }
+        public string? Nacionalidad { get; set; }
+        public string? Telefono { get; set; }
 
         public Autor? Autor { get; set; }
         public Cliente? Cliente { get; set; }
---
+++ Libreria/CLogica/Metodos/CRUDAutor.cs
@@ -0,0 +1,19 @@
+﻿using CDatos.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLogica.Metodos
+{
+    internal class CRUDAutor
+    {
+        LibreriaContext context = new LibreriaContext();
+
+        public void AltaAuror()
+        {
+
+        }
+    }
+}
